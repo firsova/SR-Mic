687d01b67703e1e846092ec8bae82c89aef3a5af
firsova <firsova-petrsu@mail.ru>
2017-03-17 10:16:16 +0300

    -
    android_0.9.0

---
+++ app/build/generated/source/buildConfig/debug/petrsu/smartroom/android/srcli/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package petrsu.smartroom.android.srcli;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "petrsu.smartroom.android.srcli";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 14092;
+  public static final String VERSION_NAME = "";
+}
