ee8d262c0223e16f8bae761b40b46619869f070f
firsova <firsova-petrsu@mail.ru>
2017-04-22 21:05:32 +0300

    ADDED: qlistView

--- app/src/main/java/petrsu/smartroom/android/srcli/QueueList.java
+++ app/src/main/java/petrsu/smartroom/android/srcli/QueueList.java
@@ -4,18 +4,37 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user on 11.05.16.
  */
 public class QueueList  extends ActionBarActivity implements AdapterView.OnItemLongClickListener {
+
     private ListView qlistView;
+    ArrayAdapter<String> arr;
+    List<String> q;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.queue_list);
 
         qlistView = (ListView) findViewById(R.id.qlistView);
+        q = new ArrayList<>();
+        qlistView.setAdapter(arr);
+        qlistView.setOnItemLongClickListener(this);
+
+        //вывести реквесты из сиба по одному в список
+        for (int x = 0; x < KP.getRequestCount(); x = x + 1) {
+            q.add(KP.getRequestList(x));
+        }
+
+        arr = new ArrayAdapter<String>(this, R.layout.list_item, R.id.name, q);
+        qlistView.setAdapter(arr);
     }
 
 
@@ -27,5 +46,3 @@ public class QueueList  extends ActionBarActivity implements AdapterView.OnItemL
     }
 
 }
-
-
