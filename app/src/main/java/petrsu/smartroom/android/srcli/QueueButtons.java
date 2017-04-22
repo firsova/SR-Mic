f3b999ac38f489ad9ca8b804815f69a9f4646670
firsova <firsova-petrsu@mail.ru>
2017-04-22 21:08:19 +0300

    ADDED: buttons onClick & onCreate

--- app/src/main/java/petrsu/smartroom/android/srcli/QueueButtons.java
+++ app/src/main/java/petrsu/smartroom/android/srcli/QueueButtons.java
@@ -1,5 +1,6 @@
 package petrsu.smartroom.android.srcli;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -15,23 +16,88 @@ public class QueueButtons  extends ActionBarActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.queue_buttons);
-        
+
+        //задать кнопки и их обработчики
         toQueue = (Button) findViewById (R.id.toQueue);
         toQueue.setOnClickListener(this);
         exitQueue = (Button) findViewById (R.id.exitQueue);
         exitQueue.setOnClickListener(this);
+
+        //если реквест уже был отправлен
+        if (KP.existingRequest(KP.gettingUsername) == 0)
+        {
+            //нельзя нажать кнопку подачи заявки
+            exitQueue.setBackgroundColor(getResources().getColor(R.color.gray));
+            exitQueue.setEnabled(false);
+        } else
+            {
+                 //иначе нельзя отменить заявку
+                 toQueue.setBackgroundColor(getResources().getColor(R.color.gray));
+                 toQueue.setEnabled(false);
+            }
     }
+
+
+
+    //обработать нажатие кнопок
     public void onClick(View v){
 
         switch(v.getId()) {
 
-      /* toQueue button */
+            //обработать кнопку подачи заявки
             case R.id.toQueue:
+
+                //послать реквест в сиб
+                if (KP.registerRequest(KP.gettingUsername) == 0)
+                {
+                    //проверить вывод
+                    System.out.println("REQUEST FROM " + KP.gettingUsername + " ADDED");
+
+                    //изменить цвет кнопок после нажатия
+                    toQueue.setBackgroundColor(getResources().getColor(R.color.gray));
+                    toQueue.setEnabled(false);
+                    exitQueue.setBackgroundColor(getResources().getColor(R.color.md_red_A400));
+                    exitQueue.setEnabled(true);
+
+                    //открыть вкладку очереди
+                    Intent intentQueueList = new Intent();
+                    intentQueueList.setClass(this, QueueList.class);
+                    startActivity(intentQueueList);
+                } else{
+                    System.out.println("ERROR!!! CANT ADD REQUEST");
+                }
 
                 break;
 
-      /* exitQueue button */
+            //обработать кнопку удаления заявки
             case R.id.exitQueue:
+
+                //проверить существует ли реквест от данного пользователя
+                if (KP.existingRequest(KP.gettingUsername) == 1)
+                {
+                    //если существует, удалить реквест из сиба
+                    if (KP.deleteRequest(KP.gettingUsername) == 0)
+                    {
+                        System.out.println("REQUEST FROM" + KP.gettingUsername + "DELETEd");
+                    } else{
+                        System.out.println("ERROR!!! CANT DELETE REQUEST");
+                    }
+                }
+                //если не существует - заблокировать эту кнопку для нажатия
+
+                //проверить вывод
+
+                //изменить цвет кнопок после нажатия
+                toQueue.setBackgroundColor(getResources().getColor(R.color.md_light_green_A700));
+                toQueue.setEnabled(true);
+                exitQueue.setBackgroundColor(getResources().getColor(R.color.gray));
+                exitQueue.setEnabled(false);
+
+                //открыть вкладку очереди
+                Intent intentQueueList = new Intent();
+                intentQueueList.setClass(this, QueueList.class);
+                startActivity(intentQueueList);
+
 
                 break;
         }
